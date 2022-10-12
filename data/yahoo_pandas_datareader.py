2d8d528b2886fdb50d0b5779e40622234e7d86e0
hujinfeng <pbfoxhu@gmail.com>
2022-10-12 09:54:05 +0800

    pandas_datareader数据

--- data/yahoo_pandas_datareader.py
+++ data/yahoo_pandas_datareader.py
@@ -2,8 +2,10 @@ from pandas_datareader import data as pdr
 
 import yfinance as yf
 
+# 通过yfinance下载
 yf.pdr_override()  # <== that's all it takes :-)
 
+# 作者说这种方式加载数据较快
 # download dataframe
 data = pdr.get_data_yahoo("AAPL", start="2021-01-01", end="2022-10-30")
 
