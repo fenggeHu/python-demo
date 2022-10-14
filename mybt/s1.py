c9c22a1d730868b159e8559d762c052f7229c648
hujinfeng <pbfoxhu@gmail.com>
2022-10-14 15:02:11 +0800

    test

--- mybt/s1.py
+++ mybt/s1.py
@@ -3,13 +3,17 @@ import backtrader.indicators as btind
 
 
 class MyStrategy(bt.Strategy):
-
     def __init__(self):
         self.sma = btind.SimpleMovingAverage(period=15)
+
+    def log(self, txt):
+        dt = self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
 
     def next(self):
         print(self.sma)
         print(self.data.close)
+        self.log('Close, %.2f' % self.data.close[0])
         if self.sma > self.data.close:
             # Do something
 
