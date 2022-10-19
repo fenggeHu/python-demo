c0142a17786d0c241e591817538cfd28ed388386
hujinfeng <pbfoxhu@gmail.com>
2022-10-19 09:41:55 +0800

    parse eastmoney

--- data/feeder.py
+++ data/feeder.py
@@ -1,3 +1,20 @@
-class Feeder:
+import datetime
+import requests
+import pandas as pd
+import eastmoney as em
+
+
+class DataFeeder:
+    def get(self, dataname: str, start=None, end=None):
+        pass
+
     def store(self, dataname: str):
         pass
+
+
+class RemoteDataFeeder(DataFeeder):
+    def get(self, dataname: str, start=None, end=None):
+        json = em.cn_chartbar(dataname, start, end)
+        klines = json['data']['klines']
+        df = pd.DataFrame(klines, columns=['date','open','close','high','low','volume','turnover','amplitude','chg_pct','chg','turnover_rate'])
+        return df
