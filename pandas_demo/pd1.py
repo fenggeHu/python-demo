62e38c926521c7a4796b0792c3bf7e11fa3adfcf
hujinfeng <pbfoxhu@gmail.com>
2022-10-25 13:29:00 +0800

    test backtesting

--- pandas_demo/pd1.py
+++ pandas_demo/pd1.py
@@ -1,6 +1,10 @@
 # %%
+from numbers import Number
+
 import pandas as pd
 from data import local_data_path
+
+from typing import Sequence
 
 # read csv data
 # pd.read_csv(data, index_col='年份') # 指定列名
@@ -10,5 +14,17 @@ from data import local_data_path
 
 symbol = 'SZ300760'
 ohlcv = pd.read_csv(local_data_path + symbol + "/day.csv", index_col='datetime').loc['2022-09-01':'2022-09-30']
+ohlcv.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
 print(ohlcv)
+
+
 # %%
+def gets(series1: Sequence):
+    return (
+        series1.values if isinstance(series1, pd.Series) else
+        (series1, series1) if isinstance(series1, Number) else
+        series1)
+
+
+series = gets(ohlcv)
+print(series)
